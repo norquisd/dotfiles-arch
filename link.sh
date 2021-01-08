57a5cae92bbb66b1d8896a9671c62be26e89658e
Quinten Norton <quinten.norton@gmail.com>
2021-01-07 18:35:45 -0600

    push

--- link.sh
+++ link.sh
@@ -3,7 +3,7 @@
 DOTFILES="/home/quinn/.dotfiles"
 
 # neovim
-ln -s $DOTFILES/nvim ~/.config/nvim
+ln -sfn $DOTFILES/nvim ~/.config/nvim
 
 # zsh
 ln $DOTFILES/.zshrc ~/.zshrc
@@ -13,26 +13,26 @@ ln $DOTFILES/.zprofile ~/.zprofile
 ln $DOTFILES/.xinitrc ~/.xinitrc
 
 #bspwm
-ln -s $DOTFILES/bspwm ~/.config/bspwm
-ln -s $DOTFILES/sxhkd ~/.config/sxhkd
+ln -sfn $DOTFILES/bspwm ~/.config/bspwm
+ln -sfn $DOTFILES/sxhkd ~/.config/sxhkd
 
 #i3
-ln -s $DOTFILES/i3 ~/.config/i3
+ln -sfn $DOTFILES/i3 ~/.config/i3
 
 #dunst
-ln -s $DOTFILES/dunst ~/.config/dunst
+ln -sfn $DOTFILES/dunst ~/.config/dunst
 
 #polybar
-ln -s $DOTFILES/polybar ~/.config/polybar
+ln -sfn $DOTFILES/polybar ~/.config/polybar
 
 #ranger
-ln -s $DOTFILES/ranger ~/.config/ranger
+ln -sfn $DOTFILES/ranger ~/.config/ranger
 
 #zathura
-ln -s $DOTFILES/zathura ~/.config/zathura
+ln -sfn $DOTFILES/zathura ~/.config/zathura
 
 #picom
 ln $DOTFILES/picom.conf ~/.config/picom.conf
 
 #kitty
-ln -s $DOTFILES/kitty ~/.config/kitty
+ln -sfn $DOTFILES/kitty ~/.config/kitty
